04d2bdf3d5a8144b683409b865e2ff0c288470f4
OskarAlyn <alyn0001@umn.edu>
2020-01-23 10:15:03 -0600

    seperate effective distance into its own function

--- modules/classes/Graphic.js
+++ modules/classes/Graphic.js
@@ -40,12 +40,35 @@ export class Graphic {
     return returnGraphic
   }
 
+  // finds the  distance from the camera for perspective factors
+  findEffectiveDistance(display, aGameObject, part) {
+    // find effective distance for perspective
+    let cameraDistance  = display.cameraDistance;
+    let relevantCameraX = display.cameraX - cameraDistance * Math.cos(Math.PI * -0.5 + display.cameraR);
+    let relevantCameraY = display.cameraY - cameraDistance * Math.sin(Math.PI * -0.5 + display.cameraR);
+    let relevantCameraZ =                   cameraDistance * Math.sin(Math.PI * 0.5 + display.cameraTilt);
+
+    let physCoordinate = rotateCoord(part, aGameObject.r);
+
+    let xDistance = aGameObject.x + (physCoordinate[0] * aGameObject.scale) - relevantCameraX;
+    let yDistance = aGameObject.y + (physCoordinate[1] * aGameObject.scale) - relevantCameraY;
+    let zDistance = aGameObject.z + (physCoordinate[2] * aGameObject.scale) - relevantCameraZ;
+
+    let angleFromCameraHorizontal = (Math.PI * -0.5 + angleTo(xDistance, yDistance, 0 , 0)) - display.cameraR;
+    let angleFromCameraVertical = Math.PI * 0.5 * (1 - display.cameraTilt);
+
+    let horizontalDistance = pythagorean(xDistance, yDistance, 0, 0);
+    let effectiveDistance = (Math.cos(angleFromCameraVertical) * Math.cos(angleFromCameraHorizontal) * horizontalDistance);
+    effectiveDistance -= zDistance * Math.sin(angleFromCameraVertical);
+
+    return effectiveDistance;
+  }
+
   // draws the graphic to the canvas
   draw(aGameObject, display) {
-    let pastCoordinate;
-    let newCoordinate;
+    let pastCoordinate, newCoordinate; // vars for line ends
 
-    // setup adjusted variables for relative camera
+    // setup adjusted variables for relative camera position
     let scaledGraphic = this.scaleGraphic(this.parts, (aGameObject.scale * display.worldScale));
     let adjustedPosition = rotateCoord([aGameObject.x - display.cameraX, aGameObject.y - display.cameraY, aGameObject.z], -1 * display.cameraR);
     let adjustedX = adjustedPosition[0] * display.worldScale;
@@ -53,62 +76,30 @@ export class Graphic {
     let adjustedZ = adjustedPosition[2] * display.worldScale;
     let adjustedR = aGameObject.r - display.cameraR;
 
-    // showing shields
-    // context.globalAlpha = (typeof aGameObject.shield !== 'undefined' ? aGameObject.shield / aGameObject.shieldMax : 1);
-
-    // draw the graphic
+    // generate the lines
     for (let i = 0; i < scaledGraphic.length; i++) {
-      let currentPart = scaledGraphic[i];
-      let currentCoordinate = rotateCoord(currentPart[0], adjustedR);
+      for (let ii = 0; ii < scaledGraphic[i].length; ii++) {
+        let effectiveDistance = this.findEffectiveDistance(display, aGameObject, this.parts[i][ii]);
+        let currentCoordinate = rotateCoord(scaledGraphic[i][ii], adjustedR);
 
-      // actual drawing
-      for (let ii = 0; ii < currentPart.length; ii++) {
-
-        // find effective distance for perspective
-        let cameraDistance  = display.cameraDistance;
-        let relevantCameraX = display.cameraX - cameraDistance * Math.cos(Math.PI * -0.5 + display.cameraR);
-        let relevantCameraY = display.cameraY - cameraDistance * Math.sin(Math.PI * -0.5 + display.cameraR);
-        let relevantCameraZ =                   cameraDistance * Math.sin(Math.PI * 0.5 + display.cameraTilt);
-
-        let physCoordinate = rotateCoord(this.parts[i][ii], aGameObject.r);
-
-        let xDistance = aGameObject.x + (physCoordinate[0] * aGameObject.scale) - relevantCameraX;
-        let yDistance = aGameObject.y + (physCoordinate[1] * aGameObject.scale) - relevantCameraY;
-        let zDistance = aGameObject.z + (physCoordinate[2] * aGameObject.scale) - relevantCameraZ;
-
-        let angleFromCameraHorizontal = (Math.PI * -0.5 + angleTo(xDistance, yDistance, 0 , 0)) - display.cameraR;
-        let angleFromCameraVertical = Math.PI * 0.5 * (1 - display.cameraTilt);
-
-        let horizontalDistance = pythagorean(xDistance, yDistance, 0, 0);
-        let effectiveDistance = (Math.cos(angleFromCameraVertical) * Math.cos(angleFromCameraHorizontal) * horizontalDistance);
-        effectiveDistance -= zDistance * Math.sin(angleFromCameraVertical)
-
-        currentCoordinate = rotateCoord(currentPart[ii], adjustedR);
+        // calculate the two coordinates
         let xCoord = adjustedX + currentCoordinate[0];
-
-        xCoord *= (
-          effectiveDistance > 0
-          ? 1 / (1 + effectiveDistance * 0.008)
-          : 1 + effectiveDistance * -0.008 // @Todo make accurate
-        );
-
+        xCoord *= 1 / (1 + effectiveDistance * 0.008); // @Todo make accurate?
         xCoord += display.x0;
+
         let yCoord = display.yFactor * (adjustedY + currentCoordinate[1]);
         yCoord -= (adjustedZ + currentCoordinate[2]) * display.zFactor;
-
-        yCoord *= (
-          effectiveDistance > 0
-          ? 1 / (1 + effectiveDistance * 0.008)
-          : 1 + effectiveDistance * -0.008 // @Todo make accurate?
-        );
-
+        yCoord *= 1 / (1 + effectiveDistance * 0.008); // @Todo make accurate?
         yCoord += display.y0;
 
+        // if on screen, return the line to display
         if (ii == 0 || effectiveDistance < 0) {
           newCoordinate = [xCoord, yCoord, effectiveDistance];
         } else {
           pastCoordinate = [newCoordinate[0], newCoordinate[1], newCoordinate[2]];
           newCoordinate = [xCoord, yCoord, effectiveDistance];
+
+          // format line for display
           display.toDraw[display.toDraw.length] = {
             x1: pastCoordinate[0],
             y1: pastCoordinate[1],
@@ -122,17 +113,5 @@ export class Graphic {
         }
       }
     }
-
-    // // draw text
-    // if (display.debugMode) {
-    //   // context.fillStyle = aGameObject.color;
-    //   // context.font = "30px Arial";
-    //   if ( typeof aGameObject.text !== 'undefined' ) {
-    //     // context.fillText(
-    //   //     aGameObject.text,
-    //   //     display.x0 + adjustedX - 8,
-    //   //     20 + (this.maxPoint * aGameObject.scale * display.worldScale) + display.y0 + (display.yFactor * adjustedY));
-    //   // }
-    // }
   }
 }
